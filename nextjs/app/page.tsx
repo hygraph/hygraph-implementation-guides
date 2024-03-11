5c8a489b880b81c323fff079d0fac9adac97b165
Tim Benniks <tbenniks@gmail.com>
2024-03-11 16:00:23 +0100

    Next dynamic pages added

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,34 +1,48 @@
-async function getPage(slug: string) {
+import Link from "next/link";
+
+async function getPages() {
   const response = await fetch(process.env.NEXT_HYGRAPH_ENDPOINT, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      query: `query Page($slug:String!) {
-        page(where: {slug: $slug}) {
+      query: `query Pages {
+        pages {
           title
+          slug
           body {
             text
           }
         }
       }`,
-      variables: {
-        slug,
-      },
     }),
   });
   const json = await response.json();
-  return json.data.page;
+  return json.data.pages;
 }
 
 export default async function Home() {
-  const page = await getPage("hygraph-works-with-any-framework");
+  const pages = await getPages();
   return (
-    <div className="px-5 mt-10">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-5xl font-bold">{page.title}</h1>
-        <p className="text-lg">{page.body.text}</p>
+    <div className="m-12">
+      <h1 className="text-5xl font-bold mb-4">
+        Hygraph Implementation Guides demo
+      </h1>
+      <p className="text-lg mb-4">Click the links below to see other pages</p>
+      <ul className="mb-8 list-disc list-inside">
+        {pages.map((page: any) => {
+          return (
+            <li key={page.slug}>
+              <Link href={`/page/${page.slug}`} className="underline">
+                {page.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <p>
         <a
           className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 my-6 rounded"
           href="https://hygraph.com/docs/implementations"
@@ -36,7 +50,7 @@ export default async function Home() {
         >
           Implementation Docs
         </a>
-      </div>
+      </p>
     </div>
   );
 }
